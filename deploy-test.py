7d06c1aa26fb3160298775a35a682562f9d24903
Tom Galindo <98626996+thommodin@users.noreply.github.com>
2026-05-26 13:15:48 +1000

    deploy with no use

--- deploy-test.py
+++ deploy-test.py
@@ -1,15 +1,9 @@
 import prefect
+import prefect_github
 
-from data_index.batch_partitioner.greedy import GreedyBatchPartitioner
 from data_index.cluster.docker_image import DockerImage
 from data_index.cluster.fargate_cluster_config import PrefectFargateClusterConfig
 from data_index.cluster.orchestrate import orchestrate
-from data_index.file_fetcher import S3Fetcher, S5CMDFetcher, ThresholdFileFetcher
-from data_index.inventory_source.live_s3 import LiveS3InventorySource
-from data_index.metadata_extractor import NetCDFExtractor, UnstructuedNetCDFExtractor
-from data_index.structured_sink import StructuredS3TableSink
-from data_index.unstructured_metadata import InMemoryUnstructuredMetadata
-from data_index.unstructured_sink import UnstructuredS3TableSink
 
 # --- Docker images ---
 
@@ -38,32 +32,32 @@ fargate_config = PrefectFargateClusterConfig(
 
 @prefect.flow
 def data_index_blank_implementation(
-    inventory_source: LiveS3InventorySource,
-    partitioner: GreedyBatchPartitioner,
-    fetcher: S5CMDFetcher | S3Fetcher | ThresholdFileFetcher,
-    extractor: NetCDFExtractor | UnstructuedNetCDFExtractor,
-    structured_sink: StructuredS3TableSink,
-    unstructured_sink: UnstructuredS3TableSink,
+    # inventory_source: LiveS3InventorySource,
+    # partitioner: GreedyBatchPartitioner,
+    # fetcher: S5CMDFetcher | S3Fetcher | ThresholdFileFetcher,
+    # extractor: NetCDFExtractor | UnstructuedNetCDFExtractor,
+    # structured_sink: StructuredS3TableSink,
+    # unstructured_sink: UnstructuredS3TableSink,
 ) -> None:
 
     orchestrate(
-        inventory_source=inventory_source,
-        partitioner=partitioner,
-        fetcher=fetcher,
-        extractor=extractor,
-        structured_sink=structured_sink,
-        unstructured_sink=unstructured_sink,
-        metadata_factory=InMemoryUnstructuredMetadata,
+        # inventory_source=inventory_source,
+        # partitioner=partitioner,
+        # fetcher=fetcher,
+        # extractor=extractor,
+        # structured_sink=structured_sink,
+        # unstructured_sink=unstructured_sink,
+        # metadata_factory=InMemoryUnstructuredMetadata,
     )
 
 
-# github_repo = prefect_github.GitHubRepository.load("data-index")
-# prefect.flow.from_source(
-#     source=github_repo, entrypoint="deploy-test.py:orchestrate"
-# ).deploy(
-#     name="implementation_deployment",
-#     work_pool_name="docker",
-#     image=deployment_docker_image.PrefectDockerImage,
-#     push=False,
-#     build=False,
-# )
+github_repo = prefect_github.GitHubRepository.load("data-index")
+prefect.flow.from_source(
+    source=github_repo, entrypoint="deploy-test.py:data_index_blank_implementation"
+).deploy(
+    name="implementation_deployment",
+    work_pool_name="docker",
+    image=deployment_docker_image.PrefectDockerImage,
+    push=False,
+    build=False,
+)
