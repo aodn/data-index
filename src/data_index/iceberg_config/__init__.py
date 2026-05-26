378c2bc80fed8d579fcb9b31c6395bda1ae8065c
Tom Galindo <98626996+thommodin@users.noreply.github.com>
2026-05-26 16:08:41 +1000

    fix Live S3 Table Scan

--- src/data_index/iceberg_config/__init__.py
+++ src/data_index/iceberg_config/__init__.py
@@ -1,8 +1,10 @@
 from .catalog_config import S3TablesCatalogConfig, SqliteCatalogConfig
 from .iceberg_table_config import IcebergTableConfig
+from .table_scan_config import IcebergTableScanConfig
 
 __all__ = [
     "S3TablesCatalogConfig",
     "SqliteCatalogConfig",
     "IcebergTableConfig",
+    "IcebergTableScanConfig",
 ]
