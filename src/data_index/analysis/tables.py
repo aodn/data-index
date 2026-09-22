3323dc5662fe2b6501230e493d3754f0845dca8b
Tom Galindo <98626996+thommodin@users.noreply.github.com>
2026-09-22 15:05:10 +1000

    add `UNSTRUCTURED_METADATA` table

--- src/data_index/analysis/tables.py
+++ src/data_index/analysis/tables.py
@@ -27,6 +27,12 @@ STRUCTURED_METADATA_V6 = IcebergTableConfig(
     table_name="structured_metadata_v6",
 )
 
+UNSTRUCTURED_METADATA_TABLE = IcebergTableConfig(
+    catalog_config=analysis_warehouse.DATA_INDEX_CATALOG_CONFIG,
+    namespace="data_index",
+    table_name=f"unstructured_metadata_v{UnstructuredMetadata.SCHEMA_VERSION}",
+)
+
 LOCAL_STRUCTURED_METADATA_TABLE = IcebergTableConfig(
     catalog_config=analysis_warehouse.ANALYSIS_LOCAL_CATALOG,
     namespace="data_index",
