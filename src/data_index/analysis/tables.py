19829f3201bf3f453293895117705b025e15f383
Tom Galindo <98626996+thommodin@users.noreply.github.com>
2026-09-22 10:56:57 +1000

    add tables and warehouses

--- src/data_index/analysis/tables.py
+++ src/data_index/analysis/tables.py
@@ -16,12 +16,15 @@ IMOS_DATA_LIVE_TABLE = IcebergTableConfig(
 )
 
 STRUCTURED_METADATA_V5 = IcebergTableConfig(
-    catalog_config=S3TablesCatalogConfig(
-        region="ap-southeast-2",
-        arn="arn:aws:s3tables:ap-southeast-2:704910415367:bucket/data-index",
-    ),
+    catalog_config=analysis_warehouse.DATA_INDEX_CATALOG_CONFIG,
     namespace="data_index",
     table_name="structured_metadata_v5",
+)
+
+STRUCTURED_METADATA_V6 = IcebergTableConfig(
+    catalog_config=analysis_warehouse.DATA_INDEX_CATALOG_CONFIG,
+    namespace="data_index",
+    table_name="structured_metadata_v6",
 )
 
 LOCAL_STRUCTURED_METADATA_TABLE = IcebergTableConfig(
